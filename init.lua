04e2605a576d7882571843ff5bb802eb170608a5
Corey-Burdick <coreyburdickwork@gmail.com>
2025-08-17 13:07:16 -0400

    Added Render-Markdown plugin and its dependencies

--- init.lua
+++ init.lua
@@ -81,7 +81,16 @@ require("lazy").setup({
           }
       })
       end
-    } 
+    },
+    {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+    }
   },
   -- Configure any other settings here. See the documentation for more details.
   -- automatically check for plugin updates
